b257cab2fc8b61ea874ecbd3de415eb35e037b0a
vientorepublic <markgod0419@gmail.com>
2025-09-21 15:37:13 +0900

    Add deploy script for managing Docker services with rolling updates

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+SERVICE=$1
+
+if [ "$SERVICE" = "list" ]; then
+  echo "[INFO] Services:"
+  docker compose config --services
+  exit 0
+fi
+
+
+if [ -z "$SERVICE" ]; then
+  echo "[INFO] Since the service name is not specified, a full rolling update will be performed."
+  SERVICES=$(docker compose config --services)
+  for SVC in $SERVICES; do
+    echo "[INFO] Rolling service update in progress... ($SVC)"
+    docker compose up -d --build $SVC
+  done
+else
+  echo "[INFO] Rolling update only specific services ($SERVICE)."
+  docker compose up -d --build $SERVICE
+fi
